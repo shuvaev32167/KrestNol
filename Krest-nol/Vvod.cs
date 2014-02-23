8e7d5c89355e27eb29ea30a5bf746cdf3f8ba775
Vladimir <shuvaev32167@gmail.com>
2014-02-23 15:20:40 +0400

    Добавлен обработчик ввода

---
+++ Krest-nol/Vvod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KrestNol
+{
+    public class Vvod
+    {
+        public int ConvertInt(string input)
+        {
+            if (input.Split(' ').Count() != 1) 
+                return 0;
+            int value = 0;
+            Int32.TryParse(input, out value);
+            return value;
+        }
+
+        public int[] ConvertArrInt(string input)
+        {
+            if (input.Split(' ').Count() != 2) 
+                return null;
+            int[] value = new int[2];
+            string[] buf = input.Split(' ');
+            Int32.TryParse(buf.First(), out value[0]);
+            Int32.TryParse(buf.Last(), out value[1]);
+            return value;
+        }
+    }
+}
